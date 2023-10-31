f21ff197eaf378cb0ddc7c2e7a39d939f04cc25d
GUchechi <godswilluchendu7@gmail.com>
2023-10-31 23:00:58 +0100

    protecting routes

--- client/components/nav/ScreensNav.js
+++ client/components/nav/ScreensNav.js
@@ -1,19 +1,30 @@
+import { useContext } from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Signin from "../../screens/Signin";
 import Signup from "../../screens/Signup";
 import Home from "../../screens/Home";
+import { AuthContext } from "../../context/auth";
 
 const Stack = createNativeStackNavigator();
 
 export default function ScreensNav() {
+  const [state, setState] = useContext(AuthContext);
+
+  const autenticated = state && state.token !== "" && state.user !== null;
+
   return (
     <Stack.Navigator
       initialRouteName="SignIn"
       screenOptions={{ headerShown: false }}
     >
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="SignIn" component={Signin} />
-      <Stack.Screen name="SignUp" component={Signup} />
+      {autenticated ? (
+        <Stack.Screen name="Home" component={Home} />
+      ) : (
+        <>
+          <Stack.Screen name="SignIn" component={Signin} />
+          <Stack.Screen name="SignUp" component={Signup} />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
