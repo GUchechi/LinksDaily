34d2132c8ce9bdc0a0c252f52d15482c9c5ce47a
GUchechi <godswilluchendu7@gmail.com>
2023-10-31 08:46:40 +0100

    footerTabs

--- client/screens/Home.js
+++ client/screens/Home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { AuthContext } from "../context/auth";
+import FooterTabs from "../components/nav/FooterTabs";
 
 export default function Home() {
   const [state, setState] = useContext(AuthContext);
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <View>
       <Text>{JSON.stringify(state, null, 4)}</Text>
+      <FooterTabs />
     </View>
   );
 }
