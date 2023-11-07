992fb3497a8f72e670034e11ea2506d2b38cd58b
GUchechi <godswilluchendu7@gmail.com>
2023-11-07 16:20:42 +0100

    added createlinks

--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth";
+import linkRoutes from "./routes/link";
 import connectDB from "./db/db.js";
 const morgan = require("morgan");
 
@@ -19,5 +20,6 @@ app.use(morgan("dev"));
 
 // route middlewares
 app.use("/api", authRoutes);
+app.use("/api", linkRoutes);
 
 app.listen(8000, () => console.log("Server running on port 8000"));
